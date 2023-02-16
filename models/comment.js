30ea83d1c314f555cab98240af4fff156a15eafb
difurung <dfurung@hotmail.com>
2023-02-16 14:14:51 -0500

    Added homeRoutes and model

---
+++ models/comment.js
@@ -0,0 +1,49 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+
+class Comment extends Model {}
+
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      foeignKey: true,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "post",
+        key: "id",
+      },
+    },
+
+    userName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    commentBody: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "event",
+  }
+);
+
+module.exports = Comment;
---
+++ models/post.js
@@ -0,0 +1,40 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Post extends Model {}
+
+Post.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      foeignKey: true,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    postBody: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "event",
+  }
+);
+
+module.exports = Post;
