ce1905a2d262d3d69f23029a8af7779ca9319e3a
difurung <dfurung@hotmail.com>
2023-02-19 17:30:07 -0500

    Started routing in home routes. Modified  model index.js.

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -1,20 +1,92 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const { User, Post, Comment } = require('../models');
 //const withAuth = require('../utils/auth');
 
 // Prevent non logged in users from viewing the homepage 
-// Add withAuth laterin line 6
+// Homepage Render. All posts.
 router.get('/',  async (req, res) => {
   try {  
-      res.render('homepage', {
-      
-    });
+    const postData = await Post.findAll({
+    attributes: [
+      'id',
+      'title',
+      'postBody'
+    ],
+      include : [
+    {
+      model: User,
+      attributes: ['userName']
+    },
+    {
+      model: Comment,
+      attributes: [
+        'id',
+        'commentBody',
+        'post_id',
+      ],
+      include: {
+        model: User,
+        attributes: ['userName']
+      }
+    }
+  ],
+  }); 
+
+const posts = postData.map((post) => post.get({plain:true}));
+
+
+  res.render('homepage', {
+    posts,
+    logged_in: req.session.logged_in
+  });
 
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
+// Renders a single post, to Dashboard?? 
+router.get('/post/:id', async (req, res) => {
+
+  try {
+    const postData = await Post.findOne({
+      where: {
+        id: req.params.id,
+      },
+      attributes: ["id", "title", "content", "created_at"],
+      include: [
+        {
+          model: User,
+          attributes: ["userName"],
+        },
+        {
+          model: Comment,
+          attributes: ["id", "commentBody", "post_id"],
+          include: {
+            model: User,
+            attributes: ["userName"],
+          },
+        },
+      ],
+    });
+
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    res.render("singlePost", {
+      posts,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+
+});
+
+
+
+
+
+// Login Render
 router.get('/login', (req, res) => {
   // If a session exists, redirect the request to the homepage
   try{
@@ -24,5 +96,11 @@ res.status(400).json(err);
 }
     
 });
+
+// Dashboard render /dashboard
+
+//Post render
+   //Comment render
+
 
 module.exports = router;
--- models/index.js
+++ models/index.js
@@ -1,3 +1,46 @@
-const User = require('./User');
+const User = require('./user');
+const Post = require('./post');
+const Comment = require('./comment');
 
-module.exports = { User };
+
+//Associations in the Model
+
+//Post to Comment relationshios
+Post.hasMany(Comment, {
+    foreignKey: "post_id",
+    onDelete: "CASCADE",
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: "post_id",
+    onDelete: "CASCADE"
+});
+
+//Post to User relationshios
+
+Post.belongsTo(User, {
+    foreignKey: "user_id",
+    onDelete: "CASCADE",
+});
+
+User.hasMany(Post, {
+    foreignKey: "user_id",
+    onDelete: "CASCADE",
+});
+
+//Comment to User relationshios
+
+Comment.belongsTo(User, {
+    foreignKey: "user_id",
+    onDelete: "CASCADE"
+});
+
+User.hasMany(Comment, {
+    foreignKey: "user_id",
+});
+
+
+
+
+
+module.exports = { User, Post, Comment };
